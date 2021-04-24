5506d367128043fcb524a88227a8e982c7ea0fe6
Rafhael <rafhael.oliveira@hubconexa.com>
2021-04-24 14:28:25 -0300

    Adicionado controles do player de audio

---
+++ src/pages/_app.tsx
@@ -0,0 +1,43 @@
+import '../styles/globals.scss';
+
+import Header from '../components/Header';
+import Player from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+
+import { PlayerContext } from '../context/PlayerContext';
+import { useState } from 'react';
+
+function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const play = (episode) => {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  }
+
+  const setPlayingState = (state: boolean) => {
+    setIsPlaying(state);
+  }
+
+  return (
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+      <div className={styles.wrapper}>
+          <main>
+            <Header />
+            <Component {...pageProps} />
+          </main>
+          <Player />
+      </div>
+    </PlayerContext.Provider>
+  )
+}
+
+export default MyApp
